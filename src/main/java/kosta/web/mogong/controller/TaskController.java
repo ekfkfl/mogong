934331340a0f2051bbc746e0c651fabdffc4d91a
MSDN-SPECIAL\Administrator <ki224822@gmail.com>
2017-06-09 11:24:56 +0900

    no message

--- src/main/java/kosta/web/mogong/controller/TaskController.java
+++ src/main/java/kosta/web/mogong/controller/TaskController.java
@@ -1,5 +1,11 @@
 package kosta.web.mogong.controller;
 
-public class TaskController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+public class TaskController {
+	@RequestMapping("")
+	public void task() {
+	}
 }
