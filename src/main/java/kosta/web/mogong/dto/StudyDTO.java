ffe5d2e1863994047a71fee94b3b0ae83cdbd73c
DESKTOP-JGHU4S5\diabl <linuxer7@gmail.com>
2017-06-13 11:07:11 +0900

    공통코드 변경시 시스템에 실시간으로 적용되도록 변경

--- src/main/java/kosta/web/mogong/dto/StudyDTO.java
+++ src/main/java/kosta/web/mogong/dto/StudyDTO.java
@@ -22,7 +22,6 @@ public class StudyDTO {
 	private MemberDTO memberDTO; //멤버 DTO
 	
 	private CommCodeDTO commDTO; //공통 코드 DTO
-<<<<<<< HEAD
 	
 	@Override
 	public String toString() {
@@ -32,8 +31,6 @@ public class StudyDTO {
 				+ ", area=" + area + ", description=" + description + ", read=" + read + ", studyStatus=" + studyStatus
 				+ "]";
 	}
-=======
->>>>>>> Gwanng
 
 	public StudyDTO() {}
 
@@ -204,8 +201,4 @@ public class StudyDTO {
 	public void setCommDTO(CommCodeDTO commDTO) {
 		this.commDTO = commDTO;
 	}
-<<<<<<< HEAD
-=======
-
->>>>>>> Gwanng
 }
